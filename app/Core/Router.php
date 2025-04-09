b71e5ab250338fec07a39e95c66bc3b3af2a43f4
Ygor Portes <ygorportes3@gmail.com>
2025-04-09 18:21:47 -0300

    feat: add routes /login and /usuarios

--- app/Core/Router.php
+++ app/Core/Router.php
@@ -1,8 +1,33 @@
 <?php
 
-namespace Core;
+namespace App\Core;
+
+use App\Controllers\AuthController;
+use App\Controllers\UserController;
 
 class Router
 {
+    public function handleRequest()
+    {
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $uri = rtrim($uri, '/');
 
+        switch ($uri) {
+            case '':
+            case '/':
+                echo "Home";
+                break;
+            case '/usuarios':
+                $controller = new UserController();
+                $controller->index();
+                break;
+            case '/login':
+                $controller = new AuthController();
+                $controller->login();
+                break;
+            default:
+                http_response_code(404);
+                echo "Página não encontrada: $uri";
+        }
+    }
 }
--- public/index.php
+++ public/index.php
@@ -1,1 +1,8 @@
 <?php
+
+require_once  __DIR__ . '/../vendor/autoload.php';
+
+use App\Core\Router;
+
+$router = new Router();
+$router->handleRequest();
