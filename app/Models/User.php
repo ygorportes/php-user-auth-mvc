05013e66064cc583f066a5b43473fdbe3e675c72
Ygor Portes <ygorportes3@gmail.com>
2025-04-10 12:17:58 -0300

    feat: implement JSON file persistence for user data

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,14 +4,31 @@ namespace App\Models;
 
 class User
 {
+    private static $file = __DIR__ . "/../../storage/users.json";
+
+    private static function loadData()
+    {
+        if (!file_exists(self::$file)) {
+            file_put_contents(self::$file, json_encode([]));
+        }
+
+        $json = file_get_contents(self::$file);
+        return json_decode($json, true);
+    }
+
+    private static function saveData($data)
+    {
+        file_put_contents(self::$file, json_encode($data, JSON_PRETTY_PRINT));
+    }
+
     public static function all()
     {
-        return $_SESSION['users'];
+        return self::loadData();
     }
 
     public static function store($name, $email)
     {
-        $users = $_SESSION['users'] ?? [];
+        $users = self::loadData();
 
         $lastId = 0;
         if (!empty($users)) {
@@ -25,12 +42,12 @@ class User
             'email' => $email
         ];
 
-        $_SESSION['users'] = $users;
+        self::saveData($users);
     }
 
     public static function find($id)
     {
-        $users = $_SESSION['users'] ?? [];
+        $users = self::loadData();
 
         foreach ($users as $user) {
             if ($user['id'] == $id) {
@@ -43,35 +60,33 @@ class User
 
     public static function update($id, $name, $email)
     {
-        if (!isset($_SESSION['users'])) {
-            return;
-        };
+        $users = self::loadData();
 
-        $updateUsers = [];
-
-        foreach ($_SESSION['users'] as $user) {
+        foreach ($users as $index => $user) {
             if ((int)$user['id'] === (int)$id) {
-                $user['name'] = $name;
-                $user['email'] = $email;
+                $users[$index]['name'] = $name;
+                $users[$index]['email'] = $email;
+                break;
             }
-            $updateUsers[] = $user;
         }
 
-        $_SESSION['users'] = $updateUsers;
+        self::saveData($users);
     }
 
     public static function delete($id)
     {
-        $users = $_SESSION['users'] ?? [];
+        $users = self::loadData();
 
         $users = array_filter($users, fn($user) => $user['id'] != $id);
 
-        $_SESSION['users'] = array_values($users);
+        $users = array_values($users);
+
+        self::saveData($users);
     }
 
     public static function existsByEmail($email, $ignoreId = null)
     {
-        $users = $_SESSION['users'] ?? [];
+        $users = self::loadData();
 
         foreach ($users as $user) {
             if ($user['email'] === $email && $user['id'] != $ignoreId) {
